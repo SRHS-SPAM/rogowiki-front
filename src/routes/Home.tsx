cdaf4a230938831118805ccaa658714ed9fad2d5
정윤환 <torychu0315@naver.com>
2024-02-26 18:45:17 +0900

    summary

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,12 @@
+import DocumentMain from "../components/documentMain";
 import Opentab from "../components/opentab";
 
 export default function Home() {
-  return <Opentab />;
+  return (
+    <DocumentMain category={["사건", "의지평선너머로"]}>
+      <Opentab />
+      <Opentab />
+      <Opentab />
+    </DocumentMain>
+  );
 }
