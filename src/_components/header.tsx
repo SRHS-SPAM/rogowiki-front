c0ce13c8fb774be7c0f7000aa6b38a452caff2af
asdf1596 <7082mc@naver.com>
2024-02-26 23:20:33 +0900

    test

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -23,8 +23,7 @@ const Header = () => {
                     </div>
                 </div>
                 <div className="log">
-                    <button className="e">log In</button>
-                    <button className="e">sign In</button>
+                    <button className="e">로그인</button>
                 </div>
             </div>
             {/* <div className="navsub">
