357f7746309d41adf2ed94fee16bd90bc090659a
정윤환 <torychu0315@naver.com>
2024-02-26 16:15:06 +0900

    opentab done

--- src/components/opentab.tsx
+++ src/components/opentab.tsx
@@ -14,12 +14,17 @@ function Opentab() {
     <div className={styles.mainbox}>
       <div className={styles.topbox} onClick={handleTopboxClick}>
         <div className={`${styles.open_arrow} ${isOpen ? styles.rotated : ""}`}>
-          <FontAwesomeIcon icon={faChevronRight} />
+          <FontAwesomeIcon icon={faChevronRight} style={{ color: "#757575" }} />
         </div>
         <div className={styles.open_title}>논란</div>
       </div>
       <div className={`${styles.botbox} ${isOpen ? styles.open : ""}`}>
-        대충 설명
+        {isOpen && (
+          <>
+            밤 새고 잠깐 의자에서 졸다가 작업하는거 진짜 이게 맞나 모르겠다
+            졸리고 힘들고 슬프고 외롭구나...
+          </>
+        )}
       </div>
     </div>
   );
