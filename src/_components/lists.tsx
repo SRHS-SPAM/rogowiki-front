0ca1ba04ff3a33f6c2bb26c95a828d82e22d91c5
asdf1596 <7082mc@naver.com>
2024-02-28 01:47:18 +0900

    test

--- src/_components/lists.tsx
+++ src/_components/lists.tsx
@@ -1,21 +1,43 @@
 import styles from "../styles/lists.module.css";
-import React from "react";
+import useComponentSize from "../tool/size";
 
 interface ListsProps {
     lists: string[];
 }
+
 const Lists = ({ lists }: ListsProps) => {
+    const [componentRef, size] = useComponentSize();
+
+    const renderContent = () => {
+        if (size.width > 800) {
+            return (
+                <div className={styles.listMain}>
+                    <div className={styles.listTitle}>글 목록</div>
+                    {lists.map((item, index) => (
+                        <div key={index} className={styles.listList}>
+                            {item}
+                        </div>
+                    ))}
+                </div>
+            );
+        } else {
+            return (
+                <div className={styles.listMain2}>
+                    <div className={styles.listTitle2}>글 목록</div>
+                    {lists.map((item, index) => (
+                        <div key={index} className={styles.listList2}>
+                            {item}
+                        </div>
+                    ))}
+                </div>
+            );
+        }
+    };
+
     return (
-        <>
-            <div className={styles.listMain}>
-                <div className={styles.listTitle}>글 목록</div>
-                {lists.map((item, index) => (
-                    <div key={index} className={styles.listList}>
-                        {item}
-                    </div>
-                ))}
-            </div>
-        </>
+        <div className={styles.listMain} ref={componentRef}>
+            {renderContent()}
+        </div>
     );
 };
 
