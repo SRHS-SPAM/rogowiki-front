4e5bfc74f06fe55e130eb6967872102e58b67177
정윤환 <torychu0315@naver.com>
2024-02-28 15:42:08 +0900

    sadasd

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+<<<<<<< HEAD
 import React from "react";
 import logo from "  ./logo.svg";
+=======
+"use client";
+
+>>>>>>> test
 import Home from "./routes/Home";
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="App">
       <Home />
     </div>
+=======
+    <>
+      <Home />
+    </>
+>>>>>>> test
   );
 }
 
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import DocumentMain from "../components/documentMain";
 import Opentab from "../components/opentab";
 
@@ -10,3 +11,38 @@ export default function Home() {
     </DocumentMain>
   );
 }
+=======
+"use client";
+
+import "../App.css";
+import Footer from "../_components/footer";
+import Header from "../_components/header";
+import Lists from "../_components/lists";
+import MainBox from "../_components/mainbox";
+
+const lists = [
+    "제목",
+    "제목2",
+    "제목3",
+    "제목 고민중",
+    "제목 길게하기 테스트중",
+    "제목 여기까지도 가능한가 테스트중",
+    "방금전게 줄였을때 한계가 맞는듯함",
+    "제목3",
+];
+function Home() {
+    return (
+        <>
+            <Header />
+            <div className="main">
+                {/* <MainBox /> */}
+                <Lists lists={lists} />
+            </div>
+
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
+>>>>>>> test
