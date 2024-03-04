10c2382c74d2c2bb459330eebbf1a6068f23b6a5
정윤환 <torychu0315@naver.com>
2024-03-04 23:07:07 +0900

    ds

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import Home from "./routes/Home";
+import List from "./routes/list";
+import Student from "./routes/page";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/students" element={<Student />} />
+                    <Route path="/list" element={<List />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
