defec3d41b2772bd7eceaa032e0c4adaed25feb9
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-03-06 18:30:05 +0900

    0306

--- src/_components/view.tsx
+++ src/_components/view.tsx
@@ -1,10 +1,13 @@
 import MDEditor from "@uiw/react-md-editor";
 import React from "react";
-// import styled from "@emotion/styled";
+import styled from "@emotion/styled";
 import DocumentMain from "./documentMain";
 import ReactMarkdown from "react-markdown";
 import { useSize } from "../tool/SizeContext";
 import OpentabEmpty from "./opentabsEmpty";
+import ApiComponent from "../tool/apicall";
+import StyledTextComponent from "./titletest";
+
 const markdown = `# 논란
  -하나둘셋넷
 
@@ -17,30 +20,25 @@ const markdown = `# 논란
 
  로렘잇섬로렘잇섬로렘잇섬로렘잇섬로렘잇섬
  `;
-// const MarkDownStyle = styled.div`
-//     font-size: 1rem;
-//     line-height: 2.5rem;
-// `;
-// const BlockQuoteStyle = styled.blockquote`
-//     padding: 1rm;
-//     border: 1px dashed black;
-// `;
-// function BlockQuote(children: { children: React.ReactNode }) {
-//     return <BlockQuoteStyle>{children.children}</BlockQuoteStyle>;
-// }
+
+const MarkDownStyle = styled.div`
+  font-size: 1rem;
+  line-height: 2.5rem;
+`;
+
 const View = () => {
-    return (
-        <DocumentMain category={["학생"]} title={["국재윤"]}>
-            <OpentabEmpty title={"내용"}>
-                <OpentabEmpty title={"asdf"}></OpentabEmpty>
-                {/* <MarkDownStyle>
-                    <ReactMarkdown renderers={{ blockquote: BlockQuote }}>
-                        {markdown}
-                    </ReactMarkdown>
-                </MarkDownStyle> */}
-            </OpentabEmpty>
-        </DocumentMain>
-    );
+  return (
+    <DocumentMain category={["학생"]} title={["국재윤"]}>
+      <OpentabEmpty title={"내용"}>
+        <OpentabEmpty title={"asdf"}>
+          <MarkDownStyle>
+          <ReactMarkdown></ReactMarkdown>
+          </MarkDownStyle>
+          {/* <ApiComponent/> */}
+        </OpentabEmpty>
+      </OpentabEmpty>
+    </DocumentMain>
+  );
 };
 
 export default View;
